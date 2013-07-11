6d8c12cd6482f3f0cdd22ce1810e8fba6bde053a
gskulkarni <ubuntu@ip-10-188-30-208.us-west-1.compute.internal>
2013-07-11 07:40:29 +0000

    Testing

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
+
+var content = fs.readFileSync("index.html");
+var buffer = new Buffer(content, "utf-8");
+var output = buffer.toString("utf-8", 0, 12);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(output);
 });
 
 var port = process.env.PORT || 5000;
